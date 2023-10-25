764498e33c98fbc6041b330c7856b4d86db1c9bd
pr_veen <pr_veen@pr-veens-MacBook-Pro.local>
2023-10-25 20:15:52 +0530

    data modeling

---
+++ models/todos/user.models.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+    },
+    isActive: Boolean,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model('User', userSchema);
